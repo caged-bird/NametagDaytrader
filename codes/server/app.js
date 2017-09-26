7c82d53a9d1df9c107a0ff0fe4ab3f57b5d5af62
kashikasitaro <t-oikawa@sumilab.org>
2017-09-26 17:26:09 +0900

    Wrapped with vagrant

--- codes/server/app.js
+++ codes/server/app.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
     res.sendfile("/index.html");
 })
 
-const server = app.listen(3000, () => {
+const server = app.listen(1337, () => {
     const host = server.address().address;
     const port = server.address().port;
     console.log(`listening at http://${host}:${port}`);
